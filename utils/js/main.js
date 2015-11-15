705f0dcbd036648f76a31ffc21c4b3a32882d81a
Pieter van der Eems <pieter@eemco.nl>
2015-11-15 16:50:45 +0100

    Get random name

--- utils/js/main.js
+++ utils/js/main.js
@@ -6,6 +6,9 @@ requirejs(['pouchdb-5.1.0.min'], function (Pouchdb) {
         remoteDb = new Pouchdb('https://utils.faterpg.nl/db/utils', {skip_setup: true}),
         elements = {},
 //        sync, // handler for the replication
+        getDocObjects,
+        getRandomInt,
+        getRandomObject,
         getNames,
         startSync;
 
@@ -16,6 +19,27 @@ requirejs(['pouchdb-5.1.0.min'], function (Pouchdb) {
     elements.result = document.querySelector('ul');
 
     // **********************************************************
+    // ** Utililty functionality
+    // **********************************************************
+    getDocObjects = function (doc) {
+        var clean = [];
+        Object.keys(doc).forEach(function (elm) {
+            if (elm.substr(0, 1) !== '_') {
+                clean.push(doc[elm]);
+            }
+        });
+        return clean;
+    };
+
+    getRandomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    getRandomObject = function (obj) {
+        var keys = Object.keys(obj);
+        return obj[keys[getRandomInt(0, keys.length - 1)]];
+    };
+    // **********************************************************
     // ** Display Data
     // **********************************************************
     // Display Player information
@@ -24,10 +48,17 @@ requirejs(['pouchdb-5.1.0.min'], function (Pouchdb) {
     // **  Database interaction
     // **********************************************************
     // Get names from the local database
-    getNames = function (type) {
+    getNames = function (typeU) {
+        var type = typeU.toLowerCase();
         db.get('names').then(function (doc) {
-            //var langs;
-            console.log(doc);
+            var langs = getDocObjects(doc),
+                fnLang = langs[getRandomInt(0, langs.length - 1)],
+                lnLang = langs[getRandomInt(0, langs.length - 1)],
+                li = document.createElement('li'),
+                firstnameList;
+            firstnameList = fnLang.firstnames[type] || getRandomObject(fnLang.firstnames);
+            li.innerHTML = (firstnameList[getRandomInt(0, firstnameList.length - 1)]) + ' ' + lnLang[getRandomInt(0, lnLang.length - 1)];
+            elements.result.appendChild(li);
         }).catch(function (err) {
             console.error('Error getting names', err);
         });
