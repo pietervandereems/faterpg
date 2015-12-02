d92ee175be5e8197b9a7769355f0f86196c629b3
Pieter van der Eems <pieter@eemco.nl>
2015-12-02 21:30:54 +0100

    Create, see what a dataset and selection does

--- create/js/main.js
+++ create/js/main.js
@@ -2,4 +2,16 @@
 /*global requirejs*/
 requirejs(['pouchdb'], function (Pouchdb) {
     'use strict';
+    var elements;
+
+    elements.nav = document.querySelector('nav');
+
+    /*
+     * React to user interaction
+     */
+    elements.nav.addEventListener(function (ev) {
+        if (ev.target.dataset.section) {
+            console.log(document.getElementById(ev.target.dataset.section));
+        }
+    });
 });
