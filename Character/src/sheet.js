ab232081f09f862e3a3160b818641533a498f4b8
Pieter van der Eems <p.vandereems@vgz.nl>
2019-10-06 14:57:09 +0200

    Use the pouchdb database of the localhost pouchdb-server

--- Character/src/sheet.js
+++ Character/src/sheet.js
@@ -1,3 +1,6 @@
 import './style.css';
+import PouchDB from 'pouchdb-browser';
 
-console.log('sheet.js runs');
+const db = new PouchDB('http://localhost:5984/characters');
+
+console.log('sheet.js runs now with characters local database.', db.info());
