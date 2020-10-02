e6f099a7042e52919d1ec4eef6d01926f1aa5549
Pieter van der Eems <gpggit@eemco.nl>
2020-10-02 11:34:32 +0200

    switched to standardjs for character code

--- Character/src/sheet.js
+++ Character/src/sheet.js
@@ -1,32 +1,30 @@
-import './style.css';
-import PouchDB from 'pouchdb-browser';
+import './style.css'
+import PouchDB from 'pouchdb-browser'
 
-const db = new PouchDB('http://localhost:5984/characters');
+const db = new PouchDB('http://localhost:5984/characters')
 
-
-console.log('sheet.js runs now with characters local database.', db.info());
+console.log('sheet.js runs now with characters local database.', db.info())
 
 const getLocal = (() => db.get('localOne')
-    .then((doc) => {
-        console.log('doc', { doc, arr: Array.from(doc) });
-    })
-    .catch((err) => {
-        if (err.status === 404) {
-            db.put({
-                _id: 'localOne'
-            })
-                .then(getLocal());
-            return;
-        }
-        console.error('error getting localOne', err);
-    }))();
+  .then((doc) => {
+    console.log('doc', { doc, arr: Array.from(doc) })
+  })
+  .catch((err) => {
+    if (err.status === 404) {
+      db.put({
+        _id: 'localOne'
+      })
+        .then(getLocal())
+      return
+    }
+    console.error('error getting localOne', err)
+  }))()
 
 const idToSheetvalue = (value) => (id) => id.split(':').reduceRight((acc, item) =>
-    (Object.keys(acc).length === 0 ? { [item]: value } : { [item]: acc }), {});
+  (Object.keys(acc).length === 0 ? { [item]: value } : { [item]: acc }), {})
 
 document.querySelector('body').addEventListener('change', (change) => {
-    // skill:fair:4, value="Pilot"
-    // ['skill','fair','4'] => {skill: {fair: {4: "Pilot"}}}
-    db.get('localOne').then((doc) => db.put(Object.assign({}, doc, idToSheetvalue(change.target.value)(change.target.id))));
-});
-
+  // skill:fair:4, value="Pilot"
+  // ['skill','fair','4'] => {skill: {fair: {4: "Pilot"}}}
+  db.get('localOne').then((doc) => db.put(Object.assign({}, doc, idToSheetvalue(change.target.value)(change.target.id))))
+})
