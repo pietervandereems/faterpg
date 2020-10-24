ffc249f307ffd4b827f47eaf45e61ef5bb55e695
Pieter van der Eems <gpggit@eemco.nl>
2020-10-24 17:11:59 +0200

    Add tests for the dice module

--- player/tests/dice.test.js
+++ player/tests/dice.test.js
@@ -1,12 +1,39 @@
-import dice from '../dice'
+import { rollResult, fudgeSet } from '../dice.js'
+import { repeat } from '../utils/repeat'
 
 describe('roll them dice', () => {
   const rollDie = (result) => ({ roll: () => result })
   const rollPlus = rollDie('+')
+  const rollMin = rollDie('-')
+  const rollBlank = rollDie('')
 
   describe('fudge', () => {
-    test('4 +\'s should equal 4', () => {
-      expect(dice.rollResult([rollPlus, rollPlus, rollPlus], rollPlus)).toEqual(4)
+    test('+ + + + should equal 4', () => {
+      expect(rollResult([rollPlus, rollPlus, rollPlus, rollPlus]))
+        .toEqual(4)
+    })
+
+    test('+ - blank blank should equal 0', () => {
+      expect(rollResult([rollPlus, rollMin, rollBlank, rollBlank]))
+        .toEqual(0)
+    })
+
+    test('A fudgeset should contain 4 dice', () => {
+      expect(fudgeSet.length)
+        .toEqual(4)
+    })
+
+    test('A fudgeset result should be between -4 and 4', () => {
+      const fudgeRoll = rollResult(fudgeSet)
+      const roll100 = repeat(100, fudgeRoll)
+
+      roll100.forEach((roll) => {
+        expect(roll)
+          .toBeGreaterThanOrEqual(-4)
+
+        expect(roll)
+          .toBeLessThanOrEqual(4)
+      })
     })
   })
 })
